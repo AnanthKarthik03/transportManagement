166427ef910070955098a8c5818e11be9ae2deb4
rajatkumarbatham <rajat.kumar@rostantechnologies.com>
2023-04-03 17:49:26 +0530

    consigner-validation

--- src/app/validation/validation.ts
+++ src/app/validation/validation.ts
@@ -1,22 +1,22 @@
-import { FormControl } from "@angular/forms"
+import { FormControl } from '@angular/forms';
 
 export class Validation {
   isUserValid(control: FormControl) {
-      const value = control?.value;
-      const pattern = /^(?:\d{10}|\w+@\w+\.\w{3})$/;
-      if (value != '') {
-        const bool = pattern.test(value);
-        return (bool == false ? {'notValid': true} : null);
-      }
-      return null;
+    const value = control?.value;
+    const pattern = /^(?:\d{10}|\w+@\w+\.\w{3})$/;
+    if (value != '') {
+      const bool = pattern.test(value);
+      return bool == false ? { userNotValid: true } : null;
+    }
+    return null;
   }
 
   isPasswordValid(control: FormControl) {
     const value = control?.value;
     const pattern = /^(?=.*\d).{3,8}$/;
-    if(value != '') {
+    if (value != '') {
       const bool = pattern.test(value);
-      return (bool == false ? {'notValid': true} : null);
+      return bool == false ? { passwordNotValid: true } : null;
     }
     return null;
   }
