1f0c35b5b7ebd8347ff35cce3bf23e05e405db69
rajatkumarbatham <rajat.kumar@rostantechnologies.com>
2023-04-10 14:35:12 +0530

    'after-add-delivery-push'

--- src/app/settings/company/company.component.ts
+++ src/app/settings/company/company.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class CompanyComponent {
   show_form: boolean = false;
+
+  submit_to_update: boolean = false;
+
+  onEdit() {
+    this.show_form = !this.show_form;
+    this.submit_to_update = !this.submit_to_update;
+  }
 }
